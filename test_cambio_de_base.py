bcfde17b48e131d977c56f7762237552109e174b
Juan Martín Barrios <j.m.barrios@gmail.com>
2022-01-17 23:51:03 -0600

    Se corrige la prueba de cambio de base

--- test_cambio_de_base.py
+++ test_cambio_de_base.py
@@ -1,10 +1,13 @@
 import pytest
+import numpy as np
+from numpy.testing import assert_array_equal
 import homework
 
 
 @pytest.mark.parametrize('base, vector, expected',[
-    ([[0,1],[1,1]], [1,3], [2,1]),
-    ([[0,1,1],[1,0,1],[1,1,0]], [1,0,-1], [-1,0,1])
+    (np.array([[0,1],[1,1]]), np.array([1,3]), np.array([3,4])),
+    (np.array([[0,1,1],[1,0,1],[1,1,0]]), np.array([1,0,-1]), np.array([-1,0,1]))
 ])
-def cambio_de_base(base, vector, expected):
-    assert homework.cambio_de_base(base, vector) == expected
+def test_cambio_de_base(base, vector, expected):
+    assert_array_equal(homework.cambio_de_base(base, vector), expected)
+
